f9ad20b26f46adc75ada6608e2c12201666101c0
moiseshilario <moiseshilariu@gmail.com>
2019-05-23 11:33:58 -0300

    Update initial screen and set routes

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import '~/config/ReactotronConfig';
+import '~/config/DevtoolsConfig';
+
+import createNavigator from './routes';
+
+const App = () => {
+  const [userChecked, setUserChecked] = useState(false);
+  const [userLogged, setUserLogged] = useState(false);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const username = await AsyncStorage.getItem('@Githuber:username');
+      setUserChecked(true);
+      setUserLogged(!!username);
+    };
+
+    getUser();
+  }, [userLogged]);
+
+  if (!userChecked) return null;
+
+  const Routes = createNavigator(userLogged);
+
+  return <Routes />;
+};
+
+export default App;
